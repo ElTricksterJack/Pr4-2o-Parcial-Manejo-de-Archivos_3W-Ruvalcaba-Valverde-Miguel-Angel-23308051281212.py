67ac3312f67fc971499eaa44739ce32d9a338633
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-24 18:54:52 -0600

    Update punto 1 y 2.py

--- punto 1 y 2.py
+++ punto 1 y 2.py
@@ -8,16 +8,5 @@ print("------------")
 print(archivo.readline())#este separa el conetedio por un espasio hasia abajo y dise un elemento.
 #Pero solo funsiona si no esta el read
 print("------------")
-archivo = open("alubno.txt.txt", "r")
-print(archivo.read(30))#con esto puedes controlar el numero de carapteres que se mostraran
-print("------------")
-archivo = open("extra.txt", "a")
-archivo.write("")
-archivo.write("algo nuevo")
-archivo.close()
-
-print("------------")
-
-print("------------")
 archivo.close()#Mink: hohohohohoh feliz nadivdad, Mr.Ink: es close no clous, Mink. Oh mmm Huhuhuhuhu feliz haloween
 #24/10/2024.... aun falta
