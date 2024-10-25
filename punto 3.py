89e4ad908ebfbfa88fe470e3352dcbcda6360c68
ElTricksterJack <a.23308051281212@cbtis128.edu.mx>
2024-10-24 18:52:11 -0600

    Create punto 3.py
    Si el archivo se encuentra en una ubicación diferente, deberá especificar la ruta del archivo, de esta manera

---
+++ punto 3.py
@@ -0,0 +1,5 @@
+expArch= open("Usar archivos lejanos/separasion/ALT","r")
+print(expArch.read())
+#con esto abres archivos desde ubicasiones lejanas, pero nesitamos otro archivo
+print("----------")
+
